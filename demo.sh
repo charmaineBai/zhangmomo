f1c5001cae05e3c45a46ef977e490caafcb1d559
charmaineBai <31880585+charmaineBai@users.noreply.github.com>
2018-06-13 12:53:49 +0800

    append echo

--- demo.sh
+++ demo.sh
@@ -2,4 +2,7 @@ mkdir demo
 cd demo
 mkdir css js
 touch index.html css/style.css js/main.js
+echo "<!DOCTYPE><title>Hello</title><h1>Hi</hi>" >index.html
+echo "h1{color:red;}" >css/style.css
+echo "var string = "Hello World" alert(string)">js/main.js
 exit
